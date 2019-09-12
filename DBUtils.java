04f2d3c2bfb2e3b25db463c21540daffbc7447d1
bishnu9776 <bishnu9776@gmail.com>
2019-09-13 00:02:54 +0530

    yest ir is reverted Revert "DButils"
    This reverts commit 5de3ed850d67f8273fa14e0a213f25732a30aec1.

--- DBUtils.java
+++ DBUtils.java
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 thesi no updatwe bishnu praswad nayak
 
 updated and modifieded now 12.09.2019 
-=======
-updated now 12.09.2019 
->>>>>>> parent of f0d6d8e... updated Dbutils.java
 hy guis this is java class, git commands are very imp
 
