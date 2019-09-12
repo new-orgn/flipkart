5de3ed850d67f8273fa14e0a213f25732a30aec1
bishnu9776 <bishnu9776@gmail.com>
2019-09-13 00:00:34 +0530

    DButils

--- DBUtils.java
+++ DBUtils.java
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 thesi no updatwe bishnu praswad nayak
 
 updated and modifieded now 12.09.2019 
+=======
+updated now 12.09.2019 
+>>>>>>> parent of f0d6d8e... updated Dbutils.java
 hy guis this is java class, git commands are very imp
 
